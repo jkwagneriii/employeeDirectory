97294296ba15ed903025722a64351cd9c8ffe3e8
Jake Wagner <jkwagneriii@gmail.com>
2020-11-18 19:07:27 -0600

    setting up api

---
+++ client/src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+//Random user generator 
+const BASE_URL = "https://randomuser.me/api/";
+
+export default {
+    search: function () {
+        return axios.get(BASE_URL)
+    }
+}
