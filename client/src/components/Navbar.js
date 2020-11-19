4a4c209f11f530cdb93b4552115aa79304a5a11c
Jake Wagner <jkwagneriii@gmail.com>
2020-11-18 18:55:28 -0600

    attempting to add bootstrap navbar to no avail - but it is close

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,1 +1,19 @@
 import React from "react";
+
+// function Navbar() {
+//     return (
+//         <React.Fragment>
+//         <div>
+//             <nav class="navbar navbar-light bg-light">
+//                 <a class="navbar-brand">Navbar</a>
+//                 <form class="form-inline">
+//                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+//                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//                 </form>
+//             </nav>
+//         </div>
+//         </React.Fragment>
+//     );
+// }
+
+export default Navbar;
